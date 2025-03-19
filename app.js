d04a507dd8e3790d09e707f8c09f6b4a278ad49f
dayul <dyulchoo@naver.com>
2025-03-19 11:36:58 +0900

    json 데이터 넘기기

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send('Hello, Express!');
+    res.send('Hello, Express! (GET) ');
+});
+
+app.post('/hi', (req, res) => {
+    res.send(req.body);
 });
 
 app.listen(PORT, () => {
